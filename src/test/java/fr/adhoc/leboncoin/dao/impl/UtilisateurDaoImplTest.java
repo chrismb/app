795e1c1287aadcd592bd6b052be8e78cb65962d7
Christopher Marshall-Breton <christopher.marshall-breton@cfa-afti.fr>
2012-11-30 10:59:57 +0100

    added myDao as attribute, and added findByName test

--- src/test/java/fr/adhoc/leboncoin/dao/impl/UtilisateurDaoImplTest.java
+++ src/test/java/fr/adhoc/leboncoin/dao/impl/UtilisateurDaoImplTest.java
@@ -22,11 +22,13 @@ import fr.adhoc.leboncoin.dao.impl.UtilisateurDaoImpl;
 public class UtilisateurDaoImplTest{
 	private static DbUtils myDbUtils;
 	private static int lastID;
+	private static UtilisateurDao myDao;
 	
 
-	@BeforeClass public static void runBeforeClass() throws Exception{
+	@BeforeClass public static void runBeforeClass() throws SQLException, Exception{
 	// run for one time before all test cases
 		myDbUtils = new DbUtils();
+		myDao = new UtilisateurDaoImpl();
 		Statement stmt = myDbUtils.getStatement();
 		ResultSet rslt = stmt.executeQuery("SELECT * FROM Utilisateur");
 		while  (rslt.next()){
@@ -38,7 +40,6 @@ public class UtilisateurDaoImplTest{
 	@Test
 	public void createTest() throws SQLException, Exception{
 		Utilisateur testut = new Utilisateur("test","test@test.ts");
-		UtilisateurDao myDao = new UtilisateurDaoImpl();
 		myDao.create(testut);
 
 		Statement stmt = myDbUtils.getStatement();
@@ -53,31 +54,45 @@ public class UtilisateurDaoImplTest{
 
 	@Test
 	public void findByIdTest() throws SQLException, Exception{
-		Utilisateur testut = new Utilisateur("test","test@test.ts");
-		UtilisateurDao myDao = new UtilisateurDaoImpl();
+		Utilisateur testut = new Utilisateur("test","test@test.ts");	
 		myDao.create(testut);
 
 		Statement stmt = myDbUtils.getStatement();
 		ResultSet rslt = stmt.executeQuery("SELECT * FROM Utilisateur WHERE NOM='test'");
 		int IDtest = 0;
 		while  (rslt.next()){
-			//On recupere le mail du dernier utilisateur ajoute (normalement "test@test.ts")
+			//On recupere l'ID' du dernier utilisateur ajoute 
 			IDtest = rslt.getInt("U_ID");
 		}
 		assertEquals(myDao.findById(IDtest).getMail(), testut.getMail());
 	}
 
 	@Test
-	public void findAllTest() throws SQLException, Exception{
-		UtilisateurDao myDao = new UtilisateurDaoImpl();
+	public void findAll() throws SQLException, Exception{
 		Statement stmt = myDbUtils.getStatement();
 		ResultSet rslt = stmt.executeQuery("SELECT * FROM Utilisateur");
 		int nbrTest = 0;
 		while  (rslt.next()){
-			//On recupere le mail du dernier utilisateur ajoute (normalement "test@test.ts")
+			//On recupere le nombre d'utilisateurs
 			nbrTest ++;
 		}
 		assertEquals(myDao.findAll().size(), nbrTest);
+	}
+
+	@Test
+	public void findByName() throws SQLException, Exception{
+		Utilisateur testut1 = new Utilisateur("test","test1@test.ts");	
+		Utilisateur testut2 = new Utilisateur("test","test2@test.ts");
+		myDao.create(testut1);	
+		myDao.create(testut2);
+		Statement stmt = myDbUtils.getStatement();
+		ResultSet rslt = stmt.executeQuery("SELECT * FROM Utilisateur WHERE NOM='test'");
+		int nbrTest = 0;
+		while  (rslt.next()){
+			//On recupere le nombre d'utilisateurs dont le nom est test
+			nbrTest ++;
+		}
+		assertEquals(myDao.findByName("test").size(), nbrTest);
 	}
 
 	@AfterClass public static void runAfterClass() throws SQLException, Exception {
